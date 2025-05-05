bf7d750fce98f955b923bfd2a93dae61a66097fc
hazeldejuan <hazeldejuan217@gmail.com>
2025-05-05 23:27:35 +0800

    Add files via upload

---
+++ PROBSET 2-1.cxx
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int isa,dalawa,tatlo,apat,lima;
+	int sum;
+	double average;
+	
+	cout << “Enter 5 integer numbers\n: ” ;
+	
+	cout << “Number 1: “;	
+	cin >> isa;
+	
+	cout << “Number 2: “;
+	cin >> dalawa;
+	
+	cout << “Number 3: “;
+	cin >> tatlo;
+	
+	cout << “Number 4: “;
+	cin >> apat;
+	
+	cout << “Number 5: “;
+	cin >> lima;
+	
+	sum = isa + dalawa + tatlo + apat+ lima;
+	average = sum/5.0;
+	
+	cout << “The average is: “ << average << endl;
+	
+	return 0;
+}
